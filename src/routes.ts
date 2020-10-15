919472eff1efd7de4aa907036f0272d24f164fff
Victor de Carvalho Silva <vtrcarvalho.13@gmail.com>
2020-10-14 21:45:26 -0300

    Upload de imagens

--- src/routes.ts
+++ src/routes.ts
@@ -1,11 +1,14 @@
 import {Router} from 'express';
+import multer from 'multer';
+
+import uploadConfig from './config/upload';
 import OrphanagesController from './controllers/OrphanagesController';
 
 const router = Router();
-
+const upload = multer(uploadConfig);
 
 router.get('/orfanatos', OrphanagesController.index);
 router.post('/orfanatos/:id', OrphanagesController.show);
-router.post('/orfanatos', OrphanagesController.create);
+router.post('/orfanatos', upload.array('images'),OrphanagesController.create);
 
 export default router;
